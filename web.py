03c8fa0fedbdf16a969ea925f26376ce9a95f459
dbha <dbha0719@gmail.com>
2022-07-05 22:01:30 +0900

    update

--- web.py
+++ web.py
@@ -9,11 +9,11 @@ app.logger.setLevel(logging.INFO)
 @app.route("/")
 def hello_world():
     app.logger.info("default path")
-    return "Hello, python demo!!"
+    return "Hello, python Demo Test!!"
 
 
 @app.route("/hello/<username>")
 def hello_user(username):
     # say hello to that user
     app.logger.info("/hello path. User: " + username)
-    return "Hello python Demo %s" % escape(username)
+    return "Hello python Demo Test %s" % escape(username)
